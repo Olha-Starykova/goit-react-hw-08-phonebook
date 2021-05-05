70ad0ee4abe4fbcd517360dbbc41d78dc621de26
Olha-Starykova <olenkalog@gmail.com>
2021-05-05 23:24:43 +0300

    com

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { PersistGate } from 'redux-persist/integration/react'
+import App from './App';
+import store from './redux/store'
+import { Provider } from 'react-redux';
+
+//console.log(store)
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={store.persistor}> */}
+        <App />
+      {/* </PersistGate> */}
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+); 
---
+++ src/redux/contacts/contacts-operations.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import {
+    addContactRequest,
+    addContactSuccess,
+    addContactError,
+    // changeFilter,
+    deleteContactRequest,
+    deleteContactSuccess,
+    deleteContactError,
+    fetchContactRequest,
+    fetchContactSuccess,
+    fetchContactError,
+} from './contacts-actions'
+
+axios.defaults.baseURL = 'http://localhost:4040'
+
+const fetchTodos = () => async dispatch => {
+    dispatch(fetchContactRequest());
+
+    try {
+        const { data } = await axios.get('/contacts');
+        dispatch(fetchContactSuccess(data))
+    } catch (error) {
+        dispatch(fetchContactError(error))
+    }
+     // axios
+    //     .get('/contacts')
+    //     .then(({ data }) => dispatch(fetchContactSuccess(data)))
+    //     .catch(error => dispatch(fetchContactError(error)));
+} 
+
+const addTodo = (name, number) => async dispatch => {
+    const contacts = { name, number }
+    dispatch(addContactRequest())
+
+    try {
+        const { data } = await axios.post('/contacts', contacts)
+        dispatch(addContactSuccess(data))
+    } catch (error) {
+        dispatch(addContactError(error))
+    }
+    // axios
+    //     .post('/contacts', contacts)
+    //     .then(({ data }) =>
+    //         dispatch(addContactSuccess(data)))
+    //     .catch(error => dispatch(addContactError(error)));
+};
+
+
+const deleteTodo = contactId =>  dispatch => {
+    dispatch(deleteContactRequest())
+
+      axios
+        .delete(`/contacts/${contactId}`)
+        .then(() => dispatch(deleteContactSuccess(contactId)))
+        .catch(error => dispatch(deleteContactError(error)));
+
+};
+    
+
+export default {
+    fetchTodos,
+    addTodo,
+    deleteTodo,
+}
