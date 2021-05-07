bbacd7a7381e1bf54d2848dc0039ec5585e4867d
Olha-Starykova <olenkalog@gmail.com>
2021-05-07 23:43:25 +0300

    com

---
+++ src/redux/auth/auth-selectors.js
@@ -0,0 +1,11 @@
+export default {};
+
+
+// const getIsAuthenticated = state => Boolean(state.auth.token);
+
+// const getUsername = state => state.auth.user.name;
+
+// export default {
+//   getIsAuthenticated,
+//   getUsername,
+// };
