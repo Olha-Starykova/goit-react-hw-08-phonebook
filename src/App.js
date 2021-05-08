4370a278a2a3c6ebdc9fa8e5617cb80a6d7cbb6a
Olha-Starykova <olenkalog@gmail.com>
2021-05-08 21:46:41 +0300

    com 14

--- src/App.js
+++ src/App.js
@@ -14,15 +14,9 @@ import LoginView from './views/LoginView'
 import { Switch, Route } from 'react-router-dom';
 import AppBar from './components/AppBar';
 import { authOperations } from './redux/auth';
+import PrivateRoute from './components/PrivateRoute'
 
 
-
-
-  // componentDidMount() {
-  //   this.props.fetchTodos();
-  // }
-  // const App = () => (
-  
 
 class App extends Component {
 
@@ -40,7 +34,7 @@ class App extends Component {
           <Route exact path="/" component={HomeView} />
           <Route path="/register" component={RegisterView} />
           <Route path="/login" component={LoginView} />
-          <Route path="/contacts" component={TodosView} /> 
+          <PrivateRoute path="/contacts" component={TodosView} redirectTo="/login" /> 
        </Switch>
       </div>
 
--- src/redux/auth/auth-reducer.js
+++ src/redux/auth/auth-reducer.js
@@ -1,11 +1,8 @@
-  
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
 import authActions from './auth-actions';
 
 const initialUserState = { name: null, email: null };
-
-
 
 const user = createReducer(initialUserState, {
   [authActions.registerSuccess]: (_, { payload }) => payload.user,
@@ -14,14 +11,11 @@ const user = createReducer(initialUserState, {
   [authActions.getCurrentUserSuccess]: (_, { payload }) => payload,
 });
 
-
 const token = createReducer(null, {
   [authActions.registerSuccess]: (_, { payload }) => payload.token,
   [authActions.loginSuccess]: (_, { payload }) => payload.token,
    [authActions.logoutSuccess]: () => null,
 });
-
-
 
 
 const setError = (_, { payload }) => payload;
